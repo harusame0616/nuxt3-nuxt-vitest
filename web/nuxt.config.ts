5ccbf1976cba9edc506743bce98659db8f305bda
Nemoto Masaharu <nemoto.masaharu.it@gmail.com>
2023-05-29 08:51:40 +0900

    モックの使用、未使用の判定追加

--- web/nuxt.config.ts
+++ web/nuxt.config.ts
@@ -3,6 +3,7 @@
 export default defineNuxtConfig({
   runtimeConfig: {
     public: {
+      useMock: process.env.NODE_ENV !== "production",
       baseUrl: "http://localhost:3000/api",
     },
   },
--- web/plugins/msw.ts
+++ web/plugins/msw.ts
@@ -1,4 +1,12 @@
 export default defineNuxtPlugin(async () => {
+  const {
+    public: { useMock },
+  } = useRuntimeConfig();
+
+  if (!useMock) {
+    return;
+  }
+
   if (process.client) {
     const { worker } = await import("~/mocks/browser");
     await worker.start();
